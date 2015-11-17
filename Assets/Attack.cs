841e47594b9b0f715a3d602ee3bed1bd77671501
Robert Cole <robert-cole@hotmail.com>
2015-11-17 19:34:13 +0000

    Demo Scene Start
    Started on demo scene

---
+++ Assets/Attack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Attack : MonoBehaviour {
+
+    // Use this for initialization
+    void getAttack() {
+
+    }
+
+    void Start () {
+        string cardName = "";
+        getAttack(cardName);
+	}
+
+    private void getAttack(object cardName)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Card.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+    string cardName;
+    int cardPower, cardToughness;
+    public Card(string name, int power, int toughness)
+    {
+        this.cardName = name;
+        this.cardPower = power;
+        this.cardToughness = toughness;
+    }
+    
+        // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
